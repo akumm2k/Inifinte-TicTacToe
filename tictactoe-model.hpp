6e3302e58ce4121aeccff5bbfb107fb26b9e92ce
akumm2k <82381989+akumm2k@users.noreply.github.com>
2022-08-16 23:44:16 +0200

    Fix tiles-to-win and win check bugs
    The game failed when there's only 1 tile needed to win.
    The win check loop was insufficient, and produced a bug with the
    following inputs:
    1 1
    2 2
    0 0
    3 3

--- tictactoe-model.hpp
+++ tictactoe-model.hpp
@@ -7,21 +7,23 @@
 struct hash_pair {
     template <class T1, class T2>
     size_t operator()(const std::pair<T1, T2>& p) const {
-        auto hash1 = std::hash<T1>{}(p.first);
-        auto hash2 = std::hash<T2>{}(p.second);
+        auto hash1 {std::hash<T1>{}(p.first)};
+        auto hash2 {std::hash<T2>{}(p.second)};
         return hash1 ^ hash2;
     }
 };
 
 class TicTacToe {
-	using board_type = std::unordered_map<point_type, char, hash_pair>;
-
-	uint8_t player = 1;
+	using player_type = uint8_t;
+	using board_type = std::unordered_map<point_type, player_type, hash_pair>;
+	using winner_type = int8_t;
+	
+	player_type player {1};
 	const size_t tiles_to_win;
 	board_type board;
 
-	char winner;
-	const std::vector<std::pair<char, char>> dirs 
+	winner_type winner;
+	const std::vector<std::pair<int8_t, int8_t>> dirs 
 	{{0, 1}, // right 
 	{-1, 1}, // up and right
 	{-1, 0}, // up 
@@ -33,29 +35,41 @@ class TicTacToe {
 		return board.find(point) != board.end();
 	}
 
+	void print_board() {
+		for (const auto &[pnt, pl] : board)
+			std::cout << pnt.first << "," << pnt.second << ": " << static_cast<int>(pl) << "\n";
+	}
+
 	bool check_win(const point_type& point) { 
-		size_t countr;
+		print_board();
+		if (tiles_to_win == 1 && board_contains(point)) {
+			// why play this game?
+			line = WinLine(1);
+			line.add(point);
+			return true; 
+		}
+
 		for (auto &&dir : dirs) {
-			const char dx = dir.first, dy = dir.second;
-			countr = 0;
-			int x = point.first - tiles_to_win * dx, 
-				y = point.second  - tiles_to_win * dy; 
+			const int8_t dx {dir.first}, dy {dir.second};
+			size_t countr {0};
+			int x {static_cast<int>(point.first - tiles_to_win * dx)}, 
+				y {static_cast<int>(point.second - tiles_to_win * dy)}; 
 
 			WinLine new_line(tiles_to_win);
-			size_t loop = 2 * tiles_to_win - 1; 
 			// check for ${tiles_to_win} consecutive plays in 
-			// a line of (2 * tiles_to_win + 1) tiles
+			// a line of (2 * tiles_to_win) tiles
+			size_t loop {2 * tiles_to_win}; 
 			
 			while (loop > 0) { 
 				if (board_contains({x, y}) && board[{x, y}] == player) {
 					new_line.add({x, y});
 					countr++;
+					if (countr == tiles_to_win) {
+						line = (new_line); 
+						return true;
+					}
 				}
 				else countr = 0;
-				if (countr == tiles_to_win) {
-					line = (new_line); 
-					return true;
-				}
 				x +=  dx; y +=  dy;
 				loop--;
 			}
@@ -63,35 +77,35 @@ class TicTacToe {
 		return false;
 	}	
 
-	public:
+public:
 
-		TicTacToe(const size_t tiles_to_win = 5) : 
-			tiles_to_win(tiles_to_win), winner(-1) { }
-		
-		uint8_t get_turn() const noexcept {
-			return player;
-		}
+	TicTacToe(const size_t tiles_to_win = 5) : 
+		tiles_to_win(tiles_to_win), winner(-1) { }
+	
+	player_type get_turn() const noexcept {
+		return player;
+	}
 
-		char get_winner() const noexcept {
-			return winner;
-		}
+	winner_type get_winner() const noexcept {
+		return winner;
+	}
 
-		const WinLine& get_win_line() const {
-			return line;
-		}
+	const WinLine& get_win_line() const {
+		return line;
+	}
 
-		bool has_winner() const noexcept {
-			return winner != -1;
-		}
+	bool has_winner() const noexcept {
+		return winner != -1;
+	}
 
-		bool move(const point_type& point) {
-			if (!board_contains(point)) {
-				board[point] = player;
-				if (check_win(point)) 
-					winner = player;
-				player = 1 - player;
-				return true;
-			}
-			return false;
+	bool move(const point_type& point) {
+		if (!board_contains(point)) {
+			board[point] = player;
+			if (check_win(point)) 
+				winner = player;
+			player = 1 - player;
+			return true;
 		}
+		return false;
+	}
 };
